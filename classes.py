28e6866592c5a2e3e696572a746f85b776debad4
Mayra Salazar <mayra.salazar@agilesolutions.com>
2020-09-13 03:18:53 -0600

    Herencia y Sobrecarga

--- classes.py
+++ classes.py
@@ -4,3 +4,31 @@ class Character:
        self.level = level
    def __str__(self):
     return "Nombre:{}\n Nivel:{}".format(self.name, self.level)
+
+
+class Protagonist(Character):
+    def __init__(self, name, level, exp=1, nextLevel=80,tryNumber=3):
+        super().__init__(name,level)
+        self.exp = exp
+        self.nextLevel = nextLevel
+        self.tryNumber = tryNumber
+    def __str__(self):
+        return f"Nombre:{self.name}\nNivel:{self.level}\nEXP:{self.exp}\nNext Level:{self.nextLevel}"
+    def __add__(self,exp):
+        self.exp += exp
+        if(self.exp >= self.nextLevel):
+            self.level+=1
+            print("Subiste un nivel!\n")
+            self.nextLevel+= self.nextLevel*2
+
+
+
+class Villain(Character):
+    pass
+
+personaje = Protagonist("Mayra",1,1)
+print(personaje)
+print()
+
+personaje + 80
+print(personaje)
